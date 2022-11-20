1ab2637260931037520fd4ee7605ecb6dab98d62
Dorman <dorman@wintego.com>
2022-11-20 15:46:04 +0200

    Updated discord bot with commads, still does not recognize cog commands.

---
+++ Discord_bot.py
@@ -0,0 +1,29 @@
+import discord
+import os
+
+from cogs import general_cog
+from cogs import music_cog
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+
+'load_dotenv()'
+'TOKEN = os.getenv("DISCORD_TOKEN")'
+TOKEN = "MTA0MzY3NDkwNDMyODk0MTU3OQ.GXcbgk.c2-0eakgQPCl6TSdMhrYDxxmSvxyfwbEjpNgPs"
+
+intents=discord.Intents.all()
+
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("."),
+                    description="Private Discord Bot for kekw Server",
+                    intents=intents)
+
+bot.remove_command('help')
+
+
+async def main():
+    await bot.add_cog(music_cog.Music(bot))
+    await bot.add_cog(general_cog.General(bot))
+
+bot.run(TOKEN)
